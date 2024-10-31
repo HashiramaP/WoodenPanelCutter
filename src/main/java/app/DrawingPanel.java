6546055f4373e26253c6ac0b3b13b23efa187dc5
BRITONMUVUNYI <brmuv@ulaval.ca>
2024-10-30 22:16:18 -0400

    Commit sur Interface BM

--- src/main/java/app/DrawingPanel.java
+++ src/main/java/app/DrawingPanel.java
@@ -1,3 +1,5 @@
+package app;
+
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
---
+++ src/main/java/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new CNCGUI();
+    }
+
+}
+
