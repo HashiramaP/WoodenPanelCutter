771c0a33db62f24fbbcf4c62d12d96fcb2800e99
Kris Bani <krban2@ulaval.ca>
2024-10-31 16:51:14 -0400

    Premiere foncitons utilitaire de ZoneInterdite

---
+++ src/main/java/app/ElemPanel.java
@@ -0,0 +1,14 @@
+package app;
+import java.util.UUID;
+
+public class ElemPanel {
+    private UUID id;
+
+    public ElemPanel(){
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId(){
+        return id;
+    }
+}
---
+++ src/main/java/app/ZoneInterdite.java
@@ -0,0 +1,32 @@
+package app;
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+public class ZoneInterdite extends ElemPanel {
+    private List<Dimension> m_dimension;
+    private Point point;
+
+    public boolean isPointInZone(Point p) {
+        for (Dimension d : m_dimension) {
+            if ((p.x >= 0 && p.x <= d.width) &&
+                    (p.y >= 0 && p.y <= d.height)) {
+                return true;
+            }
+            return false;
+        };
+    }
+
+        public List<ZoneInterdite> getZoneInterdite() {
+        };
+
+        public void supprimerZoneInterdite( UUID id) {
+
+        };
+
+
+
+}
+
